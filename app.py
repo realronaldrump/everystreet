49c660f52ae4e73f32f1bb7cea314efa51f3ed71
davisdeatonphotography <davis.deaton@verizon.net>
2024-09-22 16:40:53 -0500

    app.py production update

--- app.py
+++ app.py
@@ -2,32 +2,34 @@ import os
 import sys
 import asyncio
 import logging
-
 from hypercorn.asyncio import serve
 from hypercorn.config import Config as HyperConfig
 from create_app import create_app
 from utils import setup_logging
+
+# Add the project root to the Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+# Setup logging
 LOG_DIRECTORY = "logs"
 os.makedirs(LOG_DIRECTORY, exist_ok=True)
 log_file = os.path.join(LOG_DIRECTORY, "app.log")
-
 setup_logging(log_file)
 logger = logging.getLogger(__name__)
 
 
 async def run_app():
-    logger.info("Creating app...")
-    app = await create_app()
-    logger.info("App created successfully")
-
-    config_local = HyperConfig()
-    config_local.bind = ["0.0.0.0:8080"]
-    config_local.workers = 1
-    config_local.startup_timeout = 3600
-    logger.info("Starting Hypercorn server...")
     try:
+        logger.info("Creating app...")
+        app = await create_app()
+        logger.info("App created successfully")
+
+        config_local = HyperConfig()
+        config_local.bind = ["0.0.0.0:8080"]
+        config_local.workers = 1
+        config_local.startup_timeout = 3600
+
+        logger.info("Starting Hypercorn server...")
         await serve(app, config_local)
     except Exception as e:
         logger.error(f"Error starting Hypercorn server: {e}", exc_info=True)
